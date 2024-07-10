651929ff77096ade82b38ae8944009ccc5573285
Piotr Cembrowski <losttape@gmail.com>
2024-07-10 22:40:31 +0200

    address and ip added

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main(void) {
 
-    socket(AF_INET, SOCK_STREAM, 0);
+    int socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+
+    char* ip = "235.3.42.1";
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = 2000;
+    inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
+    address.sin_addr.s_addr;
+
+    connect(socket_file_descriptor, &address, sizeof address );
 
     return 0;
 }
